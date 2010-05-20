c6fd844a9fac2f9c759222c2ff88ce4cfdf2e159
Andrey Nikolaev <nikolaeff@gmail.com>
2010-05-20 18:52:38 +0400

    Basic tests added. Moar AR dependencies.

--- test/exposable_attributes_test.rb
+++ test/exposable_attributes_test.rb
@@ -1,8 +1,68 @@
 require 'test_helper'
 
+# base
+class User < ActiveRecord::Base
+end
+
+class User1 < User
+  exposable_attributes :xml, :except => [:encrypted_password]
+  exposable_attributes :json, :only => [:login]
+end
+
+class User2 < User
+  exposable_attributes :id, :login
+end
+
 class ExposableAttributesTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  setup_db
+
+  test "Four users should be created after db setup" do
+    assert_equal 4, User.count
+  end
+
+  test "User1#to_json should not expose anything except login" do
+    json = User1.first.to_json
+    assert_match /login/,                 json
+
+    assert_no_match /id/,                 json
+    assert_no_match /salt/,               json
+    assert_no_match /encrypted.password/, json
+  end
+
+  test "User1#to_xml should not expose encrypted_password" do
+    xml = User1.first.to_xml
+
+    assert_no_match /encrypted.password/, xml
+
+    assert_match /id/,                    xml
+    assert_match /login/,                 xml
+    assert_match /salt/,                  xml
+  end
+
+
+  test "User2#to_xml and to_json should expose id and login only" do
+    xml  = User2.first.to_xml
+    json = User2.last.to_json
+
+    for serialized in [xml, json] do
+      assert_match /id/,                    serialized
+      assert_match /login/,                 serialized
+      assert_no_match /salt/,               serialized
+      assert_no_match /encrypted.password/, serialized
+    end
+  end
+
+  test "Ensure that User#to_xml and User#to_json are not affected" do
+    xml  = User.first.to_xml
+    json = User.last.to_json
+
+    for serialized in [xml, json] do
+      assert_match /id/,                 serialized
+      assert_match /login/,              serialized
+      assert_match /salt/,               serialized
+      assert_match /encrypted.password/, serialized
+    end
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,24 @@ require 'active_support'
 require 'active_support/test_case'
 require 'active_record'
 require 'active_record/fixtures'
+
+require File.join(File.dirname(__FILE__), '../rails/init')
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+  ActiveRecord::Schema.define(:version => 1) do
+    create_table :users do |t|
+      t.string :login
+      t.string :encrypted_password
+      t.string :salt
+    end
+  end
+
+  4.times do |i|
+    User.create!(:login => "anonymous-#{i}",
+                 :encrypted_password => "password-#{i}",
+                 :salt => "#{i ^ 65534}")
+  end
+
+end
